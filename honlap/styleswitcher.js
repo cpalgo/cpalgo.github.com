8399f6e2772e145cba37861cff9380b051e24b29
dbence2002 <71468524+dbence2002@users.noreply.github.com>
2021-05-01 22:41:36 +0200

    Én, a bulibáró!

--- honlap/styleswitcher.js
+++ honlap/styleswitcher.js
@@ -1,67 +1,90 @@
 function setActiveStyleSheet(title) {
-  var i, a, main;
-  for(i=0; (a = document.getElementsByTagName("link")[i]); i++) {
-    if(a.getAttribute("rel").indexOf("style") != -1 && a.getAttribute("title")) {
-      a.disabled = true;
-      if(a.getAttribute("title") == title) a.disabled = false;
+
+    for (var index = 0, element; (element = document.getElementsByTagName("link")[index]); index++) {
+
+        if(element.getAttribute("rel").indexOf("style") != -1 && 
+           element.getAttribute("title")) {
+
+            element.disabled = true;
+            if(element.getAttribute("title") == title) {
+                element.disabled = false;
+            }
+        }
     }
-  }
-  if (title == "normal") {
-    document.getElementById("gomb_normal").className = "btn btn-dark my-2 my-sm-0 disabled"
-    document.getElementById("gomb_bigger").className = "btn btn-secondary my-2 my-sm-0"
-  } else {
-    document.getElementById("gomb_normal").className = "btn btn-secondary my-2 my-sm-0"
-    document.getElementById("gomb_bigger").className = "btn btn-dark my-2 my-sm-0 disabled"
-  }
-}
+    if (title == "normal") {
+        document.getElementById("gomb_normal").className = "btn btn-dark my-2 my-sm-0 disabled"
+        document.getElementById("gomb_bigger").className = "btn btn-secondary my-2 my-sm-0"
+    } else {
 
+        document.getElementById("gomb_normal").className = "btn btn-secondary my-2 my-sm-0"
+        document.getElementById("gomb_bigger").className = "btn btn-dark my-2 my-sm-0 disabled"
+    }
+}
+  
 function getActiveStyleSheet() {
-  var i, a;
-  for(i=0; (a = document.getElementsByTagName("link")[i]); i++) {
-    if(a.getAttribute("rel").indexOf("style") != -1 && a.getAttribute("title") && !a.disabled) return a.getAttribute("title");
-  }
-  return null;
-}
 
+    for (var index = 0, element; (element = document.getElementsByTagName("link")[index]); index++) {
+
+        if(element.getAttribute("rel").indexOf("style") != -1 && 
+           element.getAttribute("title") && !element.disabled) {
+            return element.getAttribute("title");
+        }
+    }
+    return null;
+}
+  
 function getPreferredStyleSheet() {
-  var i, a;
-  for(i=0; (a = document.getElementsByTagName("link")[i]); i++) {
-    if(a.getAttribute("rel").indexOf("style") != -1
-       && a.getAttribute("rel").indexOf("alt") == -1
-       && a.getAttribute("title")
-       ) return a.getAttribute("title");
-  }
-  return null;
-}
 
-function createCookie(name,value,days) {
-  if (days) {
-    var date = new Date();
-    date.setTime(date.getTime()+(days*24*60*60*1000));
-    var expires = "; expires="+date.toGMTString();
-  }
-  else expires = "";
-  document.cookie = name+"="+value+expires+"; path=/";
-}
+    for(var index = 0, element; (element = document.getElementsByTagName("link")[index]); index++) {
 
+        if(element.getAttribute("rel").indexOf("style") != -1 && 
+           element.getAttribute("rel").indexOf("alt") == -1 && 
+           element.getAttribute("title")) {
+               return element.getAttribute("title");
+        }
+    }
+    return null;
+}
+  
+function createCookie(name, value, days) {
+
+    if (days) {
+        var date = new Date(), expires = "; expires=";
+        date.setTime(date.getTime() + (days * 24 * 60 * 60 * 1000));
+        expires += date.toGMTString();
+
+    } else {
+        expires = "";
+    }
+    document.cookie = name + "=" + value + expires + "; path=/";
+}
+  
 function readCookie(name) {
-  var nameEQ = name + "=";
-  var ca = document.cookie.split(';');
-  for(var i=0;i < ca.length;i++) {
-    var c = ca[i];
-    while (c.charAt(0)==' ') c = c.substring(1,c.length);
-    if (c.indexOf(nameEQ) == 0) return c.substring(nameEQ.length,c.length);
-  }
-  return null;
-}
 
-window.onload = function(e) {
-  var cookie = readCookie("style");
-  var title = cookie ? cookie : getPreferredStyleSheet();
-  setActiveStyleSheet(title);
-}
+    var nameEQ = name + "=";
+    var tokens = document.cookie.split(';');
 
-window.onunload = function(e) {
-  var title = getActiveStyleSheet();
-  createCookie("style", title, 365);
+    for (var token in tokens) {
+
+        while (token.charAt(0) == ' ') {
+            token = token.substring(1, c.length);
+        }
+        if (token.indexOf(nameEQ) == 0) {
+            return token.substring(nameEQ.length, token.length);
+        }
+    }
+    return null;
+}
+  
+window.onload = function() {
+
+    var cookie = readCookie("style");
+    var title = cookie? cookie : getPreferredStyleSheet();
+    setActiveStyleSheet(title);
+}
+  
+window.onunload = function() {
+
+    var title = getActiveStyleSheet();
+    createCookie("style", title, 365);
 }
